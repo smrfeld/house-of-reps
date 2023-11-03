857df8ae856ccb21e916f5a699456635673f83d2
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-11-03 16:19:55 -0700

    Updated plotting

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -1,19 +1,6 @@
 import utils
-
 import houseofreps as hr
-import plotly.graph_objects as go
-from typing import List, Dict, Tuple, Optional
-from dataclasses import dataclass
-from mashumaro import DataClassDictMixin
-import os
-import numpy as np
 import argparse
-from loguru import logger
-import copy
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -21,6 +8,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("command", type=str, choices=["rpr", "rpr-frac", "pop-rankings", "rankings-fracs", "shift-pop"])
     parser.add_argument("--show", action="store_true")
+    parser.add_argument("--verbose", action="store_true")
     args = parser.parse_args()
 
     if args.command == "rpr":
@@ -42,7 +30,7 @@ if __name__ == "__main__":
 
     elif args.command == "shift-pop":
         for year in hr.Year:
-            utils.plot_shift_pop(year, args.show)
+            utils.plot_shift_pop(year, args.show, report_all=args.verbose)
 
     else:
         raise ValueError(f"Unknown command: {args.command}")
--- analysis/utils/assignments_from_pop_shifts.py
+++ analysis/utils/assignments_from_pop_shifts.py
@@ -6,6 +6,7 @@ from mashumaro import DataClassDictMixin
 from loguru import logger
 import copy
 import os
+from tqdm import tqdm
 
 
 @dataclass
@@ -66,7 +67,7 @@ def find_min_pop_shift_required_for_change_repr(
     no_reps_initial = house.states[st].no_reps.voting
 
     if target == Target.LOSE and no_reps_initial == 1:
-        logger.warning(f"Cannot lose a representative from {st} - true assignment has 1, which is the minimum.")
+        # logger.warning(f"Cannot lose a representative from {st} - true assignment has 1, which is the minimum.")
         return None
 
     pop_shift = pop_shift_millions_start
@@ -138,89 +139,47 @@ def find_min_pop_shift_required_for_change_repr_hierarchical(
     return pop_shift_required
 
 
-def plot_shift_pop(year: hr.Year, show: bool):
+def plot_shift_pop(year: hr.Year, show: bool, report_all: bool = False):
     st_list = list(hr.St)
     st_list = [st for st in st_list if st != hr.St.DISTRICT_OF_COLUMBIA]
 
-    logger.info("--- Adding a representative ---")
+    if report_all:
+        logger.info(f"--- {year.value} Adding a representative ---")
     st_to_pop_shift_for_add: Dict[hr.St,float] = {}
-    for st in st_list:
+    for st in tqdm(st_list, disable=report_all):
 
         # Three stage search: coarse to fine
         pop_shift_required = find_min_pop_shift_required_for_change_repr_hierarchical(year, st, Target.ADD)
         assert pop_shift_required is not None, "Could not find a population shift that would add a representative to %s" % st.name
         st_to_pop_shift_for_add[st] = pop_shift_required
-        logger.info("%s: %f million people need to be shifted into the state to add a representative" % (st.name, pop_shift_required))
+        if report_all:
+            logger.info(f"{year.value} {st.name}: {pop_shift_required:.6f} million people need to be shifted into the state to add a representative")
 
-    logger.info("--- Losing a representative ---")
+    if report_all:
+        logger.info(f"--- {year.value} Losing a representative ---")
     st_to_pop_shift_for_lose: Dict[hr.St,Optional[float]] = {}
-    for st in st_list:
+    for st in tqdm(st_list, disable=report_all):
         pop_shift_required = find_min_pop_shift_required_for_change_repr_hierarchical(year, st, Target.LOSE)
         st_to_pop_shift_for_lose[st] = pop_shift_required
-        if pop_shift_required is not None:
-            logger.info("%s: %f million people need to be shifted out of the state to lose a representative" % (st.name, pop_shift_required))
-        else:
-            logger.info("%s: Cannot lose a representative because it only has one." % st.name)
+        if report_all:
+            if pop_shift_required is not None:
+                logger.info(f"{year.value} {st.name}: {pop_shift_required:.6f} million people need to be shifted out of the state to lose a representative")
+            else:
+                logger.info(f"{st.name}: Cannot lose a representative because it only has one.")
+
+    # Smallest shifts to add
+    logger.info(f"--- {year.value} Smallest shifts to add a rep ---")
+    smallest = sorted(st_to_pop_shift_for_add.items(), key=lambda x: x[1])
+    for st,pop_shift in smallest[:3]:
+        logger.info(f"{year.value} {st.name}: {int(pop_shift*1e6)} people need to be shifted into the state to add a representative")
+
+    logger.info(f"--- {year.value} Smallest shifts to lose a rep ---")
+    smallest = sorted(st_to_pop_shift_for_lose.items(), key=lambda x: x[1] or -1e10, reverse=True)
+    for st,pop_shift in smallest[:3]:
+        assert pop_shift is not None
+        logger.info(f"{year.value} {st.name}: {int(pop_shift*1e6)} people need to be shifted out of the state to lose a representative")
 
     plot_add_remove_bars(year, st_to_pop_shift_for_add, st_to_pop_shift_for_lose, show)
-
-
-def plot_add_remove_scatter(year: hr.Year, st_to_pop_shift_for_add: Dict[hr.St,float], st_to_pop_shift_for_lose: Dict[hr.St,Optional[float]], show: bool):
-    st_list = list(st_to_pop_shift_for_add.keys())
-
-    # Sort states for pretty plot ordering
-    st_list.sort(key=lambda st: st_to_pop_shift_for_add[st])
-
-    # Plot
-    fig = go.Figure()
-    fig.add_trace(
-        go.Scatter(
-            x=[st.name for st in st_list],
-            y=[st_to_pop_shift_for_add[st] for st in st_list],
-            name="Add a representative",
-            mode='markers',
-            marker_color="blue",
-            marker=dict(size=10),
-        )
-    )
-    fig.add_trace(
-        go.Scatter(
-            x=[st.name for st in st_list],
-            y=[st_to_pop_shift_for_lose[st] for st in st_list],
-            name="Lose a representative",
-            mode='markers',
-            marker_color="red",
-            marker=dict(size=10),
-        )
-    )
-
-    # Horizontal line at 0
-    fig.add_trace(
-        go.Scatter(
-            x=[st.name for st in st_list],
-            y=[0 for st in st_list],
-            mode='lines',
-            showlegend=False,
-            line=dict(color="black", dash="dash")
-        )
-    )
-
-    fig.update_layout(
-        title='Population shift required to add or lose a representative (%s)' % year.value,
-        xaxis_title="State",
-        yaxis_title="Population shift required (millions)",
-        height=600,
-        width=800,
-        font=dict(size=20),
-        yaxis_range=[-0.8,0.8],
-        )
-
-    os.makedirs("plots", exist_ok=True)
-    fig.write_image(f'plots/pop_shift_add_remove_scatter_%s.jpg' % year.value)
-    logger.info(f"Saved plot to: plots/pop_shift_add_remove_scatter_{year.value}.jpg")
-
-    if show:
-        fig.show()
 
 
 def plot_add_remove_bars(year: hr.Year, st_to_pop_shift_for_add: Dict[hr.St,float], st_to_pop_shift_for_lose: Dict[hr.St,Optional[float]], show: bool):
@@ -230,19 +189,20 @@ def plot_add_remove_bars(year: hr.Year, st_to_pop_shift_for_add: Dict[hr.St,floa
     st_list.sort(key=lambda st: st_to_pop_shift_for_add[st])
 
     # Plot
+    st_names = [st.name if st_to_pop_shift_for_lose[st] is not None else st.name + "*" for st in st_list]
     fig = go.Figure()
     fig.add_trace(
         go.Bar(
-            x=[st.name for st in st_list],
+            x=st_names,
             y=[st_to_pop_shift_for_add[st] for st in st_list],
             name="Add a representative",
         )
     )
     fig.add_trace(
         go.Bar(
-            x=[st.name for st in st_list],
+            x=st_names,
             y=[st_to_pop_shift_for_lose[st] for st in st_list],
-            name="Lose a representative",
+            name="Lose a representative<br>*Only has one representative",
         )
     )
 
