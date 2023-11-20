b37ec1bc3e0acc9ce1a57dc18696aea900d6f7e0
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-11-19 19:54:42 -0800

    Added tables for minimum population shifts

--- analysis/utils/assignments_from_pop_shifts.py
+++ analysis/utils/assignments_from_pop_shifts.py
@@ -1,6 +1,6 @@
 import houseofreps as hr
 import plotly.graph_objects as go
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple, List, Union
 from loguru import logger
 import os
 from tqdm import tqdm
@@ -36,17 +36,61 @@ def plot_shift_pop(year: hr.Year, show: bool, report_all: bool = False):
 
     # Smallest shifts to add
     logger.info(f"--- {year.value} Smallest shifts to add a rep ---")
-    smallest = sorted(st_to_pop_shift_for_add.items(), key=lambda x: x[1])
-    for st,pop_shift in smallest[:3]:
+    smallest_add = sorted(st_to_pop_shift_for_add.items(), key=lambda x: x[1])
+    for st,pop_shift in smallest_add[:3]:
         logger.info(f"{year.value} {st.name}: {int(pop_shift*1e6)} people need to be shifted into the state to add a representative")
 
     logger.info(f"--- {year.value} Smallest shifts to lose a rep ---")
-    smallest = sorted(st_to_pop_shift_for_lose.items(), key=lambda x: x[1] or -1e10, reverse=True)
-    for st,pop_shift in smallest[:3]:
+    smallest_lose = sorted(st_to_pop_shift_for_lose.items(), key=lambda x: x[1] or -1e10, reverse=True)
+    for st,pop_shift in smallest_lose[:3]:
         assert pop_shift is not None
         logger.info(f"{year.value} {st.name}: {int(pop_shift*1e6)} people need to be shifted out of the state to lose a representative")
 
     plot_add_remove_bars(year, st_to_pop_shift_for_add, st_to_pop_shift_for_lose, show)
+
+    # Plot as a table
+    plot_table(5, smallest_add, True, year, show)
+    plot_table(5, smallest_lose, False, year, show)
+
+def plot_table(top_n: int, smallest: Union[List[Tuple[hr.St,Optional[float]]],List[Tuple[hr.St,float]]], is_add: bool, year: hr.Year, show: bool):
+    
+    labels, values = [],[]
+    for st,pop in smallest[:top_n]:
+        labels.append(st.name)
+        if pop is None:
+            values.append("-")
+        else:
+            num_people = str(int(1e6*pop))
+            # Add commas
+            num_people = ",".join([num_people[::-1][i:i+3] for i in range(0, len(num_people), 3)])[::-1]
+            values.append(num_people)
+    
+    fig = go.Figure()
+    fig.add_trace(
+        go.Table(
+            header=dict(values=["State", "Population (# people)"]),
+            cells=dict(values=[labels, values],
+            height=50
+            )
+        )
+    )
+
+    header = "Pop shift required to add rep." if is_add else "Pop shift required to lose rep."
+    fig.update_layout(
+        title='%s (%s)' % (header, year.value),
+        height=400,
+        width=1200,
+        font=dict(size=18),
+        )
+
+    label = "add" if is_add else "lose"
+    os.makedirs("plots", exist_ok=True)
+    fname = 'plots/pop_shift_add_remove_%s_table_%s.jpg' % (year.value, label)
+    fig.write_image(fname)
+    logger.info(f"Saved plot to: {fname}")
+
+    if show:
+        fig.show()
 
 
 def plot_add_remove_bars(year: hr.Year, st_to_pop_shift_for_add: Dict[hr.St,float], st_to_pop_shift_for_lose: Dict[hr.St,Optional[float]], show: bool):
